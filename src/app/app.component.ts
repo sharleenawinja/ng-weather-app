3798f898048e50066540c490ef9f4a581768f412
sharleenawinja <awinjasharleen@gmail.com>
2023-04-17 13:20:22 +0300

    fetching weather data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  constructor(private weather: WeatherService) {}
   title = 'ng-weather-app';
 
   city: string = '';
+  conditions = {
+    location: '',
+    temp: '',
+    wind: '',
+    humidity: '',
+  };
+
+  fetchWeatherData() {
+    this.weather.getWeatherData(this.city).subscribe(
+      (response) =>
+        (this.conditions = {
+          location: response.location.name,
+          temp: response.current.temp_c,
+          wind: response.current.wind_kph,
+          humidity: response.current.humidity,
+        })
+    );
+
+    this.city = '';
+  }
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { API_KEY } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -7,9 +9,9 @@ import { Injectable } from '@angular/core';
 export class WeatherService {
   constructor(private http: HttpClient) {}
 
-  getWeatherData(cityName: string) {
-    this.http.get(
-      `https://api.weatherapi.com/v1/current.json?key=${API_KEY}$q=${cityName}`
+  getWeatherData(cityName: string): Observable<any> {
+    return this.http.get(
+      `https://api.weatherapi.com/v1/current.json?key=66553f3f9b6c4047a2693823231704&q=${cityName}&aqi=no`
     );
   }
 }
